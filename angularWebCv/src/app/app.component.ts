76d4f61b7d2e3bd6bf17b6cfbcef140499476470
Raymond <raymond.lu@ouidou.fr>
2024-10-09 18:08:23 +0200

    add css and icons picture (not the final version )

--- angularWebCv/src/app/app.component.ts
+++ angularWebCv/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./components/navbar/navbar.component";
+import {NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, NgOptimizedImage],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
