e55fae7e6e7b22c435d431e47ef7c5eeeb6bd508
Raymond <raymond.lu@ouidou.fr>
2024-10-11 17:24:13 +0200

    create some component of the navbar

--- angularWebCv/src/app/app.routes.ts
+++ angularWebCv/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import {AboutMeComponent} from "./components/about-me/about-me.component";
+import {HomePageComponent} from "./components/home-page/home-page.component";
+import {ProjectComponent} from "./components/project/project.component";
+import {ContactComponent} from "./components/contact/contact.component";
+import {SkillsComponent} from "./components/skills/skills.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' }, // Redirige vers /home
+  {path: 'home', component: HomePageComponent},
+  {path: 'about', component: AboutMeComponent},
+  {path: 'project', component: ProjectComponent},
+  {path: 'contact', component: ContactComponent},
+  {path: 'skills', component: SkillsComponent},
+
+];
